d009a2809d7055966a346f9268c351ef7818382d
YID2021start <86122724+YID2021start@users.noreply.github.com>
2021-06-23 13:18:10 +0800

    最終

--- products.py
+++ products.py
@@ -1,35 +1,48 @@
 import os #operating system作業系統
-products = []
-if os.path.isfile('products.csv'):
-	print('水唷!找到了!')
-	with open('products.csv', 'r', encoding='utf-8') as f:
-		for line in f:         #去除/n
-			name, price = line.strip().split(',')
-			products.append([name, price])
-	print(products)
-else:
-	print('裝肖偉喔?找不到啊!')
+#讀取檔案
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue         
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    return products
+#使用者輸入
+def user_input(products):
+    while True:
+        name = input('輸入商品名稱:')
+        if name == 'q':
+            break
+        price = input('輸入商品價格:')
+        products.append([name,price])
+    return products    
+#印出
+def print_porducts(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
+#寫入檔案
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
 
 
-products = []
-while True:
-	name = input('輸入商品名稱:')
-	if name == 'q':
-		break
-	price = input('輸入商品價格:')
-	products.append([name,price])
-print(products)
-
-for p in products:
-	print(p[0], '的價格是', p[1])
 
 
-with open('products.csv', 'w') as f:
-	for p in products:
-		f.write(p[0] + '的價格是' + p[1] + '\n')
+def main():
+    products = []
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('水唷!找到了!')
+        products = read_file(filename)
+    else:
+        print('裝肖偉喔?找不到啊!')
+    products = user_input(products)
+    print_porducts(products)
+    write_file('products.csv', products)
 
-price = int(price)
-with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+if __name__ == '__main__':
+    main()
